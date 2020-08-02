e9a6341baa38943ad91cdca73bc77599b33bb238
Evgeny Rubin <evgeny.rubin90@gmail.com>
2020-08-02 22:01:42 +0300

    Updates

---
+++ precache-manifest.21380e25215392df3793232254f3a290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b156a728df4bd6840d4c2b726fac7eb",
+    "url": "/breaches/index.html"
+  },
+  {
+    "revision": "da42cc1c4bb67f0b5b3a",
+    "url": "/breaches/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a48e6427116bddce77c3",
+    "url": "/breaches/static/js/2.5633f0f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaches/static/js/2.5633f0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da42cc1c4bb67f0b5b3a",
+    "url": "/breaches/static/js/main.7376bfe8.chunk.js"
+  },
+  {
+    "revision": "3350e4f7b45d345cce03",
+    "url": "/breaches/static/js/runtime-main.8d8509f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/breaches/static/media/logo.5d5d9eef.svg"
+  }
+]);
